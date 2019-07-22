c35206559825a82855b61f2e22d89923ff1851fc
Adam Santiaji <adam.wikrama@gmail.com>
2019-07-22 17:54:09 +0700

    Updates

---
+++ precache-manifest.2133dcd6db9fae68f66245be4027ec58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1afcc19b4348d2e12ced86c6391ab7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2d809fb05c28cd675ee6",
+    "url": "/portfolio/static/css/main.ac66ff86.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "2d809fb05c28cd675ee6",
+    "url": "/portfolio/static/js/main.0c32e836.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
