dbf193a3a7d9bddae043a9a8c331d2acbcf0a3dc
Adam Santiaji <adam.wikrama@gmail.com>
2019-07-22 21:18:29 +0700

    slight change intro

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -4,23 +4,10 @@ import "./intro.scss";
 const Intro = (props) => {
     return (
         <div id="welcome" className="intro">
-            <h1>Hello World</h1>
-            <samp>console.log("Hello World");</samp>
-            <h2>
-                <p>Hi, my name is </p>
-                Raden Adam Santiaji. 
-                <p>I am a Junior Frontend developer based in</p>
-                <b>Bogor</b>. recently, i worked at &nbsp; 
-                <a
-                // eslint-disable-next-line
-                    target="_blank"
-                    href="https://kreatorku.com"
-                    alt="Kreatorku.com"
-                >
-                     Kreatorku.com
-                </a>{" "}
-                located in Tangerang.
-            </h2>
+            <h1>Junior Front-end Developer</h1>
+            <samp>I code from design mockup to web, and i love pudding :)</samp>
+            <h2>Hi, i'm Adam. Nice to meet you</h2>
+            {/* profile image*/}
         </div>
     );
 };
